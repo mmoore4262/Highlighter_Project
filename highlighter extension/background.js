3871feab62fa4e76213a882d87457f4a859d0138
f <g>
2019-03-01 10:05:47 -0500

    Check Github to see if this pushed.

--- highlighter extension/background.js
+++ highlighter extension/background.js
@@ -7,7 +7,7 @@
      chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {},
+          pageUrl: {hostEquals: "www.google.com"},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
