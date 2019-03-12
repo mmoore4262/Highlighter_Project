0bd9d3d19ea6bdc63b03962d1f863e31a3b8ef7f
f <g>
2019-03-12 09:15:37 -0400

    no push again

--- highlighter extension/options.js
+++ highlighter extension/options.js
@@ -1,4 +1,4 @@
-// Not testing 
+// Real testing branch  
   let page = document.getElementById('buttonDiv');
   const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
   function constructOptions(kButtonColors) {
